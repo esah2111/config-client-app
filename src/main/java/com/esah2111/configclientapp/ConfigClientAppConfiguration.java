6822c9a8fc56195b32adace16eeae6e50e7c1468
Mohammad Esah <esah2111@gmail.com>
2020-04-14 12:23:24 +0530

    Initial Commit

---
+++ src/main/java/com/esah2111/configclientapp/ConfigClientAppConfiguration.java
@@ -0,0 +1,21 @@
+package com.esah2111.configclientapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "some")
+public class ConfigClientAppConfiguration {
+
+	private String property;
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+	
+}
